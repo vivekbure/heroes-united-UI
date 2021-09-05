67cfeb954f482e535740e378738af2efabed5e04
Vivek Bure <vivekbure@gmail.com>
2021-09-05 11:21:07 +0530

    HU-3: Added routing

--- HeroesUnitedUI/src/app/app-routing.module.ts
+++ HeroesUnitedUI/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './pages/contact/contact.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'homepage', component: HomePageComponent },
+  {path: 'contact', component: ContactComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
